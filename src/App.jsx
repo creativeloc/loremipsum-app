5f77e3df659aa882ab0929104bbd7807c9c5c21d
Lawrence Caulker <locaulker@gmail.com>
2025-05-28 22:00:53 -0400

    setup submit for m functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react"
-import date from "./data"
+import data from "./data"
 
 const App = () => {
   const [count, setCount] = useState(1)
   const [text, setText] = useState([])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    setText(data.slice(0, amount))
+  }
+
   return (
     <section className="section-center">
       <h4>Tired Of Boring Lorem Ipsum </h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs</label>
         <input
           type="number"
@@ -24,6 +30,11 @@ const App = () => {
           Generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </section>
   )
 }
